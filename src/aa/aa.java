d9254e16b7f04cfe0575d956037e5b318e437499
KSH <KSH@DESKTOP-FJI6UJK>
2018-04-30 15:16:32 +0900

    update calendar main()

--- src/aa/aa.java
+++ src/aa/aa.java
@@ -1,13 +1,9 @@
 package aa;
 
-import java.awt.Dimension;
-
 public class aa{
 	
 	public static void main(String[] args) {
-			
-		bb mf = new bb();
-		mf.setSize(new Dimension(500, 500));
-		mf.setVisible(true);
+		System.out.println("1 2 3 4 5 6");
+		System.out.println("11 21 31 41 51 61");
 	}
 }
