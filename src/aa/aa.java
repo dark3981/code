2d1844f2d5b0c9e6d150b9012274ef685e9a5527
KSH <KSH@DESKTOP-FJI6UJK>
2018-04-30 17:15:46 +0900

    Implement find max days

--- src/aa/aa.java
+++ src/aa/aa.java
@@ -1,9 +1,18 @@
 package aa;
 
+import java.util.Scanner;
+
 public class aa{
 	
 	public static void main(String[] args) {
 		System.out.println("1 2 3 4 5 6");
 		System.out.println("11 21 31 41 51 61");
+		 
+		
+		Scanner sc = new Scanner(System.in);
+		int mt = sc.nextInt();
+		
+		int[] md = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("%d월은 %d까지 있", mt, md[mt+1]);
 	}
 }
